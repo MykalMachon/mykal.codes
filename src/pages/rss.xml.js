0d0c83b0b32ed6ddd0f57bec7735557c6afe534b
Mykal Machon <mykal@tinybox.dev>
2022-06-11 22:28:52 -0700

    fix: RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,25 @@ import rss from '@astrojs/rss';
 
 const postImportResult = import.meta.globEager('../posts/**/*.md');
 const postObjects = Object.values(postImportResult);
-const formattedPosts = postObjects.map((post) => ({
+const sortedPosts = postObjects.sort(
+  (a, b) => new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate)
+);
+
+const compiledPosts = sortedPosts.map(async (post) => ({
   link: post.frontmatter.slug,
   title: post.frontmatter.title,
   pubDate: post.frontmatter.pubDate,
+  description: `${await post.compiledContent()} <br /> <br /> <a href="${
+    import.meta.env.SITE
+  }posts/${post.frontmatter.slug}">read post on site</a>`,
 }));
 
-export const get = () =>
+export const get = async () =>
   rss({
     title: 'Mykal Machon (mykal.codes)',
     description:
       "Welcome to my site's RSS feed! I use this is a blog / place to put stuff I want to share.",
     site: import.meta.env.SITE,
     customData: `<language>en-us</language>`,
-    items: formattedPosts,
+    items: await Promise.all(compiledPosts),
   });
