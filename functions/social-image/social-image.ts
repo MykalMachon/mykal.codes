1a730973c0eed6bbd23b9d5a9e40b67f43740500
Mykal Machon <mykal@tinybox.dev>
2022-06-24 23:49:43 -0700

    fix: adding comments to social-image.ts api

--- functions/social-image/social-image.ts
+++ functions/social-image/social-image.ts
@@ -12,20 +12,35 @@ type HTMLContent = {
   image?: string;
 };
 
+/**
+ * renders a nunjucks template using the passed in template type,
+ * and the variables / html content.
+ * 
+ * @param type template name
+ * @param content content for the template
+ * @returns {Promise<string>} an html string
+ */
 const renderNunjucksTemplate = async (
   type: HTMLType,
   content: HTMLContent
 ): Promise<string> => {
-  nunjucks.configure({});
-  // get file needed
+  // get nunjucks template as text
   const fileText = await fs.readFile(`./templates/${type}.html`, {
     encoding: 'utf-8',
   });
-  // generate the file as needed
-  const generatedHTML = nunjucks.renderString(fileText, content);
-  return generatedHTML;
+
+  // render nunjucks template to string
+  nunjucks.configure({});
+  return nunjucks.renderString(fileText, content);
 };
 
+/**
+ * renders an html document in the browser and returns
+ * the screenshot as a buffer
+ * 
+ * @param htmlCode the html string to render in browser
+ * @returns {Promise<Buffer | void | undefined>} 
+ */
 const renderHTML = async (htmlCode: string): Promise<any> => {
   const browser = await puppeteer.launch({
     args: chromium.args,
@@ -39,18 +54,17 @@ const renderHTML = async (htmlCode: string): Promise<any> => {
 };
 
 export const handler: Handler = async (event, context) => {
-  // decipher passed in props
   const {
     title = 'mykal.codes',
     description = 'no description provided',
     type = 'page',
   } = event.queryStringParameters;
 
-  // generate html
+  // generate html and take screenshot
   const html = await renderNunjucksTemplate(type, { title, description });
-  // take screenshot
   const screenshot = await renderHTML(html);
 
+  // return screenshot as png
   return {
     statusCode: 200,
     headers: {
