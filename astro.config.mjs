868d6fbddfe99f053154be095008c9ec30cf12cd
Mykal Machon <mykal@tinybox.dev>
2022-07-24 13:31:57 -0700

    feat: adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config';
+// integrations
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  srcDir: "./src",
-  publicDir: "./public",
-  site: "https://mykal.codes",
+  srcDir: './src',
+  publicDir: './public',
+  site: 'https://mykal.codes',
   markdown: {
     drafts: true,
   },
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
 });
