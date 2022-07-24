f0f36457275eca9fac48fa98f3193c51364f34db
Mykal Machon <mykal@tinybox.dev>
2022-07-24 13:16:08 -0700

    fix: RSS does not show draft posts anymore

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,11 @@
 import rss from '@astrojs/rss';
+import { getNonDraftPosts } from '../utils/posts';
 
+// @ts-ignore
 const postImportResult = import.meta.globEager('../posts/**/*.md');
 const postObjects = Object.values(postImportResult);
-const sortedPosts = postObjects.sort(
+const nonDraftPosts = getNonDraftPosts(postObjects);
+const sortedPosts = nonDraftPosts.sort(
   // @ts-ignore
   (a, b) => new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate)
 );
@@ -12,6 +15,7 @@ const compiledPosts = sortedPosts.map(async (post) => ({
   title: post.frontmatter.title,
   pubDate: post.frontmatter.pubDate,
   description: `${await post.compiledContent()} <br /> <br /> <a href="${
+    // @ts-ignore
     import.meta.env.SITE
   }posts/${post.frontmatter.slug}">read post on site</a>`,
 }));
@@ -21,6 +25,7 @@ export const get = async () =>
     title: 'Mykal Machon (mykal.codes)',
     description:
       "Welcome to my site's RSS feed! I use this is a blog / place to put stuff I want to share.",
+    // @ts-ignore
     site: import.meta.env.SITE,
     customData: `<language>en-us</language>`,
     items: await Promise.all(compiledPosts),
