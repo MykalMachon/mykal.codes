e82148fb31d5e37cedc252298008fc2968bb06fa
Mykal Machon <mykal@tinybox.dev>
2023-01-25 05:06:38 +0000

    feat: moving into the garden
    moving into the garden shed

--- www/src/content/types.generated.d.ts
+++ www/src/content/types.generated.d.ts
@@ -42,331 +42,325 @@ declare module 'astro:content' {
 	};
 
 	const entryMap: {
-		"links": {
-"mykal-social.md": {
-  id: "mykal-social.md",
-  slug: "mykal-social",
-  body: string,
-  collection: "links",
-  data: any
-},
-},
-"notes": {
-"digital-gardens.md": {
-  id: "digital-gardens.md",
-  slug: "digital-gardens",
-  body: string,
-  collection: "notes",
-  data: any
-},
-},
-"photos": {
-"good-cappuccinos.md": {
-  id: "good-cappuccinos.md",
-  slug: "good-cappuccinos",
-  body: string,
-  collection: "photos",
-  data: any
-},
-"new-years-2023.md": {
-  id: "new-years-2023.md",
-  slug: "new-years-2023",
-  body: string,
-  collection: "photos",
-  data: any
-},
-"remembering-ash.md": {
-  id: "remembering-ash.md",
-  slug: "remembering-ash",
-  body: string,
-  collection: "photos",
-  data: any
-},
-},
-"posts": {
+		"posts": {
 "2022-2023.md": {
   id: "2022-2023.md",
-  slug: "2022-2023",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aeropress-latte.md": {
   id: "aeropress-latte.md",
-  slug: "aeropress-latte",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "always-take-screenshots.md": {
   id: "always-take-screenshots.md",
-  slug: "always-take-screenshots",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-1.md": {
   id: "aoc-2022-solution-for-day-1.md",
-  slug: "aoc-2022-solution-for-day-1",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-2.md": {
   id: "aoc-2022-solution-for-day-2.md",
-  slug: "aoc-2022-solution-for-day-2",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-3.md": {
   id: "aoc-2022-solution-for-day-3.md",
-  slug: "aoc-2022-solution-for-day-3",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-4.md": {
   id: "aoc-2022-solution-for-day-4.md",
-  slug: "aoc-2022-solution-for-day-4",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-5-1.md": {
   id: "aoc-2022-solution-for-day-5-1.md",
-  slug: "aoc-2022-solution-for-day-5-1",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "aoc-2022-solution-for-day-5.md": {
   id: "aoc-2022-solution-for-day-5.md",
-  slug: "aoc-2022-solution-for-day-5",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "autosave-forms.md": {
   id: "autosave-forms.md",
-  slug: "autosave-forms",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "braving-the-snow-for-bagels.md": {
   id: "braving-the-snow-for-bagels.md",
-  slug: "braving-the-snow-for-bagels",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "cauliflower-fried-rice-wraps.md": {
   id: "cauliflower-fried-rice-wraps.md",
-  slug: "cauliflower-fried-rice-wraps",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "coffee-setup.md": {
   id: "coffee-setup.md",
-  slug: "coffee-setup",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "csr-generation-scriptt.md": {
   id: "csr-generation-scriptt.md",
-  slug: "csr-generation-scriptt",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
+},
+"digital-gardens.md": {
+  id: "digital-gardens.md",
+  slug: string,
+  body: string,
+  collection: "posts",
+  data: InferEntrySchema<"posts">
 },
 "docker-rocks.md": {
   id: "docker-rocks.md",
-  slug: "docker-rocks",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "every-company-is-a-tech-company.md": {
   id: "every-company-is-a-tech-company.md",
-  slug: "every-company-is-a-tech-company",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "flipnote-hatena.md": {
   id: "flipnote-hatena.md",
-  slug: "flipnote-hatena",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
+},
+"good-cappuccinos.md": {
+  id: "good-cappuccinos.md",
+  slug: string,
+  body: string,
+  collection: "posts",
+  data: InferEntrySchema<"posts">
 },
 "hidden-content-in-an-unpublished-shopify-theme.md": {
   id: "hidden-content-in-an-unpublished-shopify-theme.md",
-  slug: "hidden-content-in-an-unpublished-shopify-theme",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "improving-anchor-links.md": {
   id: "improving-anchor-links.md",
-  slug: "improving-anchor-links",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "mit-sans-elon.md": {
   id: "mit-sans-elon.md",
-  slug: "mit-sans-elon",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
+},
+"mykal-social.md": {
+  id: "mykal-social.md",
+  slug: string,
+  body: string,
+  collection: "posts",
+  data: InferEntrySchema<"posts">
+},
+"new-years-2023.md": {
+  id: "new-years-2023.md",
+  slug: string,
+  body: string,
+  collection: "posts",
+  data: InferEntrySchema<"posts">
 },
 "on-hydroponic-gardens.md": {
   id: "on-hydroponic-gardens.md",
-  slug: "on-hydroponic-gardens",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "on-the-steam-deck.md": {
   id: "on-the-steam-deck.md",
-  slug: "on-the-steam-deck",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "optimizing-docker-containers.md": {
   id: "optimizing-docker-containers.md",
-  slug: "optimizing-docker-containers",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "pioneers-settlers-and-city-planners.md": {
   id: "pioneers-settlers-and-city-planners.md",
-  slug: "pioneers-settlers-and-city-planners",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "preparing-for-battlesnake.md": {
   id: "preparing-for-battlesnake.md",
-  slug: "preparing-for-battlesnake",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "recovering-from-failed-nextcloud-updates.md": {
   id: "recovering-from-failed-nextcloud-updates.md",
-  slug: "recovering-from-failed-nextcloud-updates",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
+},
+"remembering-ash.md": {
+  id: "remembering-ash.md",
+  slug: string,
+  body: string,
+  collection: "posts",
+  data: InferEntrySchema<"posts">
 },
 "scaling-down.md": {
   id: "scaling-down.md",
-  slug: "scaling-down",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "scheduled-python-jobs-linux-v.windows.md": {
   id: "scheduled-python-jobs-linux-v.windows.md",
-  slug: "scheduled-python-jobs-linux-vwindows",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "shopify-as-json.md": {
   id: "shopify-as-json.md",
-  slug: "shopify-as-json",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "starting-at-ufv.md": {
   id: "starting-at-ufv.md",
-  slug: "starting-at-ufv",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "tech-im-learning-this-summer-2019.md": {
   id: "tech-im-learning-this-summer-2019.md",
-  slug: "tech-im-learning-this-summer-2019",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "telegraf-and-influx-for-server-monitoring.md": {
   id: "telegraf-and-influx-for-server-monitoring.md",
-  slug: "telegraf-and-influx-for-server-monitoring",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "the-adventures-of-ash.md": {
   id: "the-adventures-of-ash.md",
-  slug: "the-adventures-of-ash",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "the-incoming-internet-switch-up.md": {
   id: "the-incoming-internet-switch-up.md",
-  slug: "the-incoming-internet-switch-up",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "tomorrow-s-legacy-software-today.md": {
   id: "tomorrow-s-legacy-software-today.md",
-  slug: "tomorrow-s-legacy-software-today",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "tracking-popular-posts.md": {
   id: "tracking-popular-posts.md",
-  slug: "tracking-popular-posts",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "web-dev-resources.md": {
   id: "web-dev-resources.md",
-  slug: "web-dev-resources",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "websites-and-wes-anderson.md": {
   id: "websites-and-wes-anderson.md",
-  slug: "websites-and-wes-anderson",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "whats-your-goto-web-stack.md": {
   id: "whats-your-goto-web-stack.md",
-  slug: "whats-your-goto-web-stack",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 "why-i-love-self-hosting.md": {
   id: "why-i-love-self-hosting.md",
-  slug: "why-i-love-self-hosting",
+  slug: string,
   body: string,
   collection: "posts",
-  data: any
+  data: InferEntrySchema<"posts">
 },
 },
 
 	};
 
-	type ContentConfig = never;
+	type ContentConfig = typeof import("./config");
 }
