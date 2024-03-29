f42b60df5740d31809d3068691e38d7d6852e9be
Mykal Machon <mykal@tinybox.dev>
2024-03-28 23:12:15 -0700

    feat: hiding snow unless it's snowing
    y'know, keep it classy. no snow in march

--- website/src/components/fun/SnowFallHeader.tsx
+++ website/src/components/fun/SnowFallHeader.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import Snowfall from "react-snowfall";
 
 const SnowFallHeader = () => {
+  const [showSnow, setShowSnow] = useState(false);
 
-  return (
-    <Snowfall color={'#fff'} radius={[0.2, 0.5]}  />
-  )
+  useEffect(() => {
+    const date = new Date();
+    const month = date.getMonth() + 1; // +1 is because jan = 0
+    setShowSnow(month < 3 || month > 8);
+  }, [])
+
+  if (showSnow) {
+    return (
+      <Snowfall color={'#fff'} radius={[0.2, 0.5]} />
+
+    )
+  }
+  else {
+    return null;
+  }
 }
 
 export default SnowFallHeader;
